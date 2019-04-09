be2c951a06a1f09e829fdf88498c4e97628183b3
Ezhu156 <ez682@nyu.edu>
2019-04-09 10:24:43 -0400

    Changes1

--- README.md
+++ README.md
@@ -11,15 +11,19 @@ This program uses the Marvel api to return a comic book based on the name of a M
 **Node Packages:** Express, ejs, body-parser, request, marvel-api, object-hash
 
 **Installing and Running the App:**
-	1. Download the files
-	2. Obtain API keys
-	3. Install node packages
-	4. Create a config file containing your API keys
-	5. Run your code
-	6. Open up localhost
+1. Download the files
+2. Obtain API keys
+3. Install node packages
+4. Create a config file containing your API keys
+5. Run your code
+6. Open up localhost
 
 
 ## Notes
 
 * Some superhero names must be typed in a particular way in order for this to work. For example, Spider Man must be spider-man, if you type it in any other way, this will not return anything. 
 * Since this returns a comic book with the superhero of your choice in it, there may be some overlap in the comic book you get. For example, if there are two characters in the same comic book, you may get the same comic book when you enter either names.
+
+## Future Fixes
+* Make it so the previous comic title isn't visible when the user clicks the submit button
+* Better error checking so that the user is aware they have entered an invalid entry
--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.get('/', function(req, res){
     res.render('index', {hero: null, error: null})
 });
 
+//Runs when user hits the submit button
+//Takes in the name the user enters and searches through the marvel api for comics with that
+//character in it and then returns a random comic out of the number of comics pulled (max = 20)
 app.post('/', function(req, res){
   let hero = req.body.hero;
-  console.log("before", hero);
-
-  // let hero = "spider-man";
   let ts = Math.floor(Date.now() / 1000);
   let num = [ts, config.publicKey, config.privateKey];
   let hashval = hash(num);
@@ -34,14 +34,15 @@ app.post('/', function(req, res){
   request(url, function(err,response,body) {
   	marvel.characters.findByName(hero)
 	  .then(function(result) {
-	  	// console.log("after", hero);
 	    console.log('Found character ID', result.data[0].id);
 	    return marvel.characters.comics(result.data[0].id);
 	  })
 	  .then(function(result) {
+	  	let randVal = Math.floor(Math.random() * result.meta.count);
+	  	// console.log("Random:", result.meta.count);
 	    console.log('found %s comics of %s total', result.meta.count, result.meta.total);
-	    console.log(result.data[Math.floor(Math.random(result.meta.count))].title);
-	    var title = result.data[Math.floor(Math.random(result.meta.count))].title;
+	    console.log(result.data[randVal].title);
+	    var title = result.data[randVal].title;
 	   	res.render('index', {hero: title, error: null})
 	  })
 	  // .fail(res.render('index', {hero: null, error: 'Error, please try again'}))
@@ -51,7 +52,7 @@ app.post('/', function(req, res){
 })
 
 
-
+//Starts the server
  app.listen(3000, function(){
     console.log('app is running on port 3000')
 })
